697121dc912b8f990310c17e24c06e5c6ac24beb
VAP810 <militarylars@outlook.com>
2019-10-19 18:54:33 +0700

    holla

---
+++ New folder/reactsocial/reactsocial/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Facebook from './components/Facebook';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-titlte">Facebook Auth Example</h1>
+      </header>
+      <p className="App-intro">to get started, authenticate with Facebook</p>
+    <Facebook />
+    </div>
+  );
+}
+
+export default App;
